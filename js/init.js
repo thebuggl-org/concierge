e4f1bf50b7ed4fab6631e0ba9f613b454fc39db0
Juan_2 <Juan_2@Juan-PC>
2015-10-30 10:00:45 -0500

    navbar effect

--- js/init.js
+++ js/init.js
@@ -1,8 +1,16 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+(function ($) {
+    $(function () {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+    }); // end of document ready
+    $(window).scroll(function () {
+        var altura=$("#barra").height();
+        var alpha=$(window).scrollTop()/altura;
+        if ($(window).scrollTop() <=altura) {
+            $("#barra").css("background-color", 'rgba(253,79,120,' + alpha/2 + ')');
+            $("#barra").html(color);
+        }else{
+            $("#barra").css({"background-color": "rgba(253,79,120," + 1 + ")"});
+        }
+    })
 })(jQuery); // end of jQuery name space
